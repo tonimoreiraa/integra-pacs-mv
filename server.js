fbd937ca9e405371c5520bbb31082bfec439bcca
Toni Moreira <moreiraatoni@gmail.com>
2024-08-09 15:05:40 -0300

    update server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-import fastify from 'fastify'
-import { initDatabase } from "./database.js";
+import fastify from 'fastify';
+import { initDatabase } from './database.js';
+import oracledb from 'oracledb'
+import fs from 'fs'
 
 const app = fastify({
     logger: true,
-})
+});
 
 function formatDateToISO(dateString) {
     const [datePart, timePart] = dateString.split(' ');
@@ -12,31 +14,99 @@ function formatDateToISO(dateString) {
 }
 
 app.post('/mv/import-report', async (req, res) => {
-    if (req.headers.authorization != process.env.API_TOKEN) {
-        return res.status(403).send('Not authorizated.')
+    if (req.headers.authorization !== process.env.API_TOKEN) {
+        return res.status(403).send({ error: 'Not authorized.' });
     }
 
-    const body = req.body
-    const database = initDatabase()
-    const gedDocumento = parseOracleRows(await database.execute(`INSERT INTO DBAMV.GED_DOCUMENTO (DS_DOCUMENTO, CD_DOCUMENTO, DT_CRIACAO, CD_VERSAO_ATUAL, CD_RESPONSAVEL_CRIACAO, CD_TIPO_DOCUMENTO, CD_SISTEMA) VALUES ('Teste', SEQ_GED_DOCUMENTO.NEXTVAL, SYSDATE, 1, 'TREINAMENTOMV', 2, 'PSDI') RETURNING CD_DOCUMENTO INTO :CD_DOCUMENTO`))[0].CD_DOCUMENTO
-    await database.execute(`INSERT INTO GED_CONTEUDO (CD_DOCUMENTO, CD_VERSAO, BLOB_CONTEUDO) VALUES (${gedDocumento}, 1, '${body.laudo_conteudo}')`)
+    const body = req.body;
+    fs.writeFile('input', JSON.stringify(body))
+    const database = initDatabase();
     
-    const formato = 'RTF'
-    const laudoDataHora = formatDateToISO(body.laudo_datahora)
+    try {
+        const result = await database.execute(
+            `INSERT INTO DBAMV.GED_DOCUMENTO 
+            (DS_DOCUMENTO, CD_DOCUMENTO, DT_CRIACAO, CD_VERSAO_ATUAL, CD_RESPONSAVEL_CRIACAO, CD_TIPO_DOCUMENTO, CD_SISTEMA) 
+            VALUES (:dsDocumento, SEQ_GED_DOCUMENTO.NEXTVAL, SYSDATE, :cdVersaoAtual, :cdResponsavelCriacao, :cdTipoDocumento, :cdSistema) 
+            RETURNING CD_DOCUMENTO INTO :cdDocumento`,
+            {
+                dsDocumento: 'Teste',
+                cdVersaoAtual: 1,
+                cdResponsavelCriacao: 'TREINAMENTOMV',
+                cdTipoDocumento: 2,
+                cdSistema: 'PSDI',
+                cdDocumento: { type: oracledb.NUMBER, dir: oracledb.BIND_OUT },
+            }
+        );
 
-    await database.execute(`INSERT INTO GED_VERSAO_DOCUMENTO (CD_DOCUMENTO, CD_VERSAO, DT_VERSAO, CD_SITUACAO, TP_FORMATO, SN_PUBLICADO) VALUES(${gedDocumento}, 1, SYSDATE, 1, '${formato}', 'N')`)
+        const gedDocumento = result.outBinds.cdDocumento[0];
 
-    await database.execute(`INSERT INTO LAUDO_RX (CD_PRESTADOR, CD_PED_RX, DT_LAUDO, SN_EMITIDO, NM_USUARIO, HR_LAUDO, CD_MULTI_EMPRESA, CD_GED_DOCUMENTO, TP_LAUDO, CD_USUARIO_ASSINADO, DH_ASSINADO, SN_ENTREGUE, SN_ASSINADO, CD_PRESTADOR_ASSINATURA, LO_ANEXO_LAUDO, TP_RESULTADO, SN_RESULTADO_PANICO) VALUES (22, ${body.accession_number}, '${laudoDataHora}', 'N', 'DBAMV', '${laudoDataHora}', 1, ${gedDocumento}, 'W', 'DBAMV', SYSDATE, 'N', 'S', 2, 'N', 'N')`)
-    
-    res.status(200).send('OK')
-})
+        await database.execute(
+            `INSERT INTO GED_CONTEUDO (CD_DOCUMENTO, CD_VERSAO, BLOB_CONTEUDO) 
+            VALUES (:cdDocumento, :cdVersao, :blobConteudo)`,
+            {
+                cdDocumento: gedDocumento,
+                cdVersao: 1,
+                blobConteudo: body.laudo_conteudo,
+            }
+        );
+
+        const formato = 'RTF';
+        const laudoDataHora = formatDateToISO(body.laudo_datahora);
+
+        // Insert into GED_VERSAO_DOCUMENTO
+        await database.execute(
+            `INSERT INTO GED_VERSAO_DOCUMENTO 
+            (CD_DOCUMENTO, CD_VERSAO, DT_VERSAO, CD_SITUACAO, TP_FORMATO, SN_PUBLICADO) 
+            VALUES (:cdDocumento, :cdVersao, SYSDATE, :cdSituacao, :tpFormato, :snPublicado)`,
+            {
+                cdDocumento: gedDocumento,
+                cdVersao: 1,
+                cdSituacao: 1,
+                tpFormato: formato,
+                snPublicado: 'N',
+            }
+        );
+
+        await database.execute(
+            `INSERT INTO LAUDO_RX 
+            (CD_PRESTADOR, CD_PED_RX, DT_LAUDO, SN_EMITIDO, NM_USUARIO, HR_LAUDO, CD_MULTI_EMPRESA, CD_GED_DOCUMENTO, TP_LAUDO, CD_USUARIO_ASSINADO, DH_ASSINADO, SN_ENTREGUE, SN_ASSINADO, CD_PRESTADOR_ASSINATURA, LO_ANEXO_LAUDO, TP_RESULTADO, SN_RESULTADO_PANICO) 
+            VALUES (:cdPrestador, :cdPedRx, :dtLaudo, :snEmitido, :nmUsuario, :hrLaudo, :cdMultiEmpresa, :cdGedDocumento, :tpLaudo, :cdUsuarioAssinado, SYSDATE, :snEntregue, :snAssinado, :cdPrestadorAssinatura, :loAnexoLaudo, :tpResultado, :snResultadoPanico)`,
+            {
+                cdPrestador: 22,
+                cdPedRx: body.accession_number,
+                dtLaudo: laudoDataHora,
+                snEmitido: 'N',
+                nmUsuario: 'DBAMV',
+                hrLaudo: laudoDataHora,
+                cdMultiEmpresa: 1,
+                cdGedDocumento: gedDocumento,
+                tpLaudo: 'W',
+                cdUsuarioAssinado: 'DBAMV',
+                snEntregue: 'N',
+                snAssinado: 'S',
+                cdPrestadorAssinatura: 2,
+                loAnexoLaudo: 'N',
+                tpResultado: 'N',
+                snResultadoPanico: 'N',
+            }
+        );
+
+        res.status(200).send('OK');
+    } catch (err) {
+        app.log.error(err);
+        res.status(500).send({ error: 'Internal Server Error' });
+    } finally {
+        await database.close();
+    }
+});
 
 const start = async () => {
     try {
-      app.listen({ port: process.env.PORT });
+        app.listen({ port: process.env.PORT, host: process.env.HOST });
     } catch (err) {
-      console.error(err);
-      process.exit(1);
+        app.log.error(err);
+        process.exit(1);
     }
-  };
-  start();
+};
+
+start();
