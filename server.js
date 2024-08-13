a46bd0df9dda69467cac923e6c06cc8eb4b3e9ad
Toni Moreira <moreiraatoni@gmail.com>
2024-08-13 11:56:08 -0300

    feat: update response message

--- server.js
+++ server.js
@@ -20,14 +20,14 @@ app.post('/mv/import-report', async (req, res) => {
     }
 
     const body = req.body;
-    fs.writeFile('input', JSON.stringify(body))
-    const database = initDatabase();
-    
+    fs.writeFileSync('./input', JSON.stringify(body))
+    const database = await initDatabase();
+
     try {
         const result = await database.execute(
-            `INSERT INTO DBAMV.GED_DOCUMENTO 
-            (DS_DOCUMENTO, CD_DOCUMENTO, DT_CRIACAO, CD_VERSAO_ATUAL, CD_RESPONSAVEL_CRIACAO, CD_TIPO_DOCUMENTO, CD_SISTEMA) 
-            VALUES (:dsDocumento, SEQ_GED_DOCUMENTO.NEXTVAL, SYSDATE, :cdVersaoAtual, :cdResponsavelCriacao, :cdTipoDocumento, :cdSistema) 
+            `INSERT INTO DBAMV.GED_DOCUMENTO
+            (DS_DOCUMENTO, CD_DOCUMENTO, DT_CRIACAO, CD_VERSAO_ATUAL, CD_RESPONSAVEL_CRIACAO, CD_TIPO_DOCUMENTO, CD_SISTEMA)
+            VALUES (:dsDocumento, SEQ_GED_DOCUMENTO.NEXTVAL, SYSDATE, :cdVersaoAtual, :cdResponsavelCriacao, :cdTipoDocumento, :cdSistema)
             RETURNING CD_DOCUMENTO INTO :cdDocumento`,
             {
                 dsDocumento: 'Teste',
@@ -36,68 +36,70 @@ app.post('/mv/import-report', async (req, res) => {
                 cdTipoDocumento: 2,
                 cdSistema: 'PSDI',
                 cdDocumento: { type: oracledb.NUMBER, dir: oracledb.BIND_OUT },
-            }
+            },
+            { autoCommit: true }
         );
 
         const gedDocumento = result.outBinds.cdDocumento[0];
+	      console.log(gedDocumento)
+
+        const formato = 'RTF';
 
         await database.execute(
-            `INSERT INTO GED_CONTEUDO (CD_DOCUMENTO, CD_VERSAO, BLOB_CONTEUDO) 
+          `INSERT INTO GED_VERSAO_DOCUMENTO
+          (CD_DOCUMENTO, CD_VERSAO, DT_VERSAO, CD_SITUACAO, TP_FORMATO, SN_PUBLICADO, CD_RESPONSAVEL)
+          VALUES (:cdDocumento, :cdVersao, SYSDATE, :cdSituacao, :tpFormato, :snPublicado, :cdResponsavel)`,
+          {
+              cdDocumento: gedDocumento,
+              cdVersao: 1,
+              cdSituacao: 1,
+              tpFormato: formato,
+              snPublicado: 'N',
+              cdResponsavel: 'DBAMV'
+          }
+      );
+
+        await database.execute(
+            `INSERT INTO GED_CONTEUDO (CD_DOCUMENTO, CD_VERSAO, BLOB_CONTEUDO)
             VALUES (:cdDocumento, :cdVersao, :blobConteudo)`,
             {
                 cdDocumento: gedDocumento,
                 cdVersao: 1,
-                blobConteudo: body.laudo_conteudo,
+                blobConteudo: { val: Buffer.from(body.laudo_conteudo, 'utf8'), type: oracledb.BLOB },
             }
         );
 
-        const formato = 'RTF';
         const laudoDataHora = formatDateToISO(body.laudo_datahora);
 
-        // Insert into GED_VERSAO_DOCUMENTO
         await database.execute(
-            `INSERT INTO GED_VERSAO_DOCUMENTO 
-            (CD_DOCUMENTO, CD_VERSAO, DT_VERSAO, CD_SITUACAO, TP_FORMATO, SN_PUBLICADO) 
-            VALUES (:cdDocumento, :cdVersao, SYSDATE, :cdSituacao, :tpFormato, :snPublicado)`,
-            {
-                cdDocumento: gedDocumento,
-                cdVersao: 1,
-                cdSituacao: 1,
-                tpFormato: formato,
-                snPublicado: 'N',
-            }
-        );
+          `INSERT INTO LAUDO_RX
+          (CD_LAUDO, CD_PRESTADOR, CD_PED_RX, DT_LAUDO, SN_EMITIDO, NM_USUARIO, HR_LAUDO, CD_MULTI_EMPRESA, CD_GED_DOCUMENTO, TP_LAUDO, CD_USUARIO_ASSINADO, DH_ASSINADO, SN_ENTREGUE, SN_ASSINADO, CD_PRESTADOR_ASSINATURA, LO_ANEXO_LAUDO, TP_RESULTADO, SN_RESULTADO_PANICO)
+          VALUES (SEQ_LAUDO_RX.NEXTVAL, :cdPrestador, :cdPedRx, TO_DATE(:dtLaudo, 'YYYY-MM-DD HH24:MI:SS'), :snEmitido, :nmUsuario, TO_DATE(:hrLaudo, 'YYYY-MM-DD HH24:MI:SS'), :cdMultiEmpresa, :cdGedDocumento, :tpLaudo, :cdUsuarioAssinado, SYSDATE, :snEntregue, :snAssinado, :cdPrestadorAssinatura, :loAnexoLaudo, :tpResultado, :snResultadoPanico)`,
+          {
+            cdPrestador: 22,
+            cdPedRx: body.accession_number,
+            dtLaudo: laudoDataHora,
+            snEmitido: 'N',
+            nmUsuario: 'DBAMV',
+            hrLaudo: laudoDataHora,
+            cdMultiEmpresa: 1,
+            cdGedDocumento: gedDocumento,
+            tpLaudo: 'W',
+            cdUsuarioAssinado: 'DBAMV',
+            snEntregue: 'N',
+            snAssinado: 'S',
+            cdPrestadorAssinatura: 2,
+            loAnexoLaudo: { type: oracledb.BLOB, dir: oracledb.BIND_OUT },
+            tpResultado: 'N',
+            snResultadoPanico: 'N'
+          }
+      )
 
-        await database.execute(
-            `INSERT INTO LAUDO_RX 
-            (CD_PRESTADOR, CD_PED_RX, DT_LAUDO, SN_EMITIDO, NM_USUARIO, HR_LAUDO, CD_MULTI_EMPRESA, CD_GED_DOCUMENTO, TP_LAUDO, CD_USUARIO_ASSINADO, DH_ASSINADO, SN_ENTREGUE, SN_ASSINADO, CD_PRESTADOR_ASSINATURA, LO_ANEXO_LAUDO, TP_RESULTADO, SN_RESULTADO_PANICO) 
-            VALUES (:cdPrestador, :cdPedRx, :dtLaudo, :snEmitido, :nmUsuario, :hrLaudo, :cdMultiEmpresa, :cdGedDocumento, :tpLaudo, :cdUsuarioAssinado, SYSDATE, :snEntregue, :snAssinado, :cdPrestadorAssinatura, :loAnexoLaudo, :tpResultado, :snResultadoPanico)`,
-            {
-                cdPrestador: 22,
-                cdPedRx: body.accession_number,
-                dtLaudo: laudoDataHora,
-                snEmitido: 'N',
-                nmUsuario: 'DBAMV',
-                hrLaudo: laudoDataHora,
-                cdMultiEmpresa: 1,
-                cdGedDocumento: gedDocumento,
-                tpLaudo: 'W',
-                cdUsuarioAssinado: 'DBAMV',
-                snEntregue: 'N',
-                snAssinado: 'S',
-                cdPrestadorAssinatura: 2,
-                loAnexoLaudo: 'N',
-                tpResultado: 'N',
-                snResultadoPanico: 'N',
-            }
-        );
-
-        res.status(200).send('OK');
+        res.status(200).send({ mensagem: 'Sucesso' });
     } catch (err) {
         app.log.error(err);
-        res.status(500).send({ error: 'Internal Server Error' });
+        res.status(500).send({ mensagem: 'Internal Server Error' });
     } finally {
-        await database.close();
     }
 });
 
